ad2e6f166793f06b949e5eaefe7a009aa763cfeb
jasminemina <mina.jas24@gmail.com>
2017-05-02 18:31:29 -0700

    Separate Student and Parent Info into their own components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
+import StudentInfo from './StudentInfo/index.jsx'
 
 
 export default class App extends React.Component {
 
+	constructor() {
+		super()
+
+		this.state = {
+			view: 'home'
+		}
+
+	}
+
+
+
+
   render() {
-    return (
-        <div style={{textAlign: 'center'}}>
-          <h1>Hurray for TechBridge</h1>
-        </div>
-      );
+
+  	if (this.state.view === 'home') {
+			return (
+				<div style={{textAlign: 'center'}}>
+	        <h1>Hurray for TechBridge</h1>
+	        <span onClick={()=>{this.setState({view: 'studentInfo'})}}>Enter Student Info</span>
+	      </div>
+			)
+  	} else if (this.state.view === 'studentInfo') {
+  		return (
+  			<StudentInfo />
+			)
+  	}
 
   }
 
